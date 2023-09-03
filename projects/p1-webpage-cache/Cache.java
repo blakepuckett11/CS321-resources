399f76b4b12583a788e5fadc6bea5d6f5f9e02e8
blakepuckett11 <blakepuckett@u.boisestate.edu>
2023-09-03 12:48:59 -0600

    Cache changes

---
+++ projects/p1-webpage-cache/Cache.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class Cache<K, V extends KeyInterface<K>> {
+
+    LinkedList<V> cacheList = new LinkedList<>();
+    
+    public V remove(K key) {
+        
+        return null;
+    }
+    public void clear() {
+
+    }
+    public String toString() {
+        return null;
+    }
+}
